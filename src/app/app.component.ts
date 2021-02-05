b6b51b180fe228a8df6c966842b513dd5781feeb
wdCassiopeia <ucakar.klavdija@gmail.com>
2021-02-05 11:33:34 +0100

    style changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { TabMenu } from 'primeng/tabmenu';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  items: MenuItem[] = [];
+  activeItem: MenuItem = { label: 'Settings', icon: 'pi pi-fw pi-cog' };
+
+  ngOnInit(): void {
+
+    this.items = [
+      { label: 'Settings', icon: 'pi pi-fw pi-cog' },
+      { label: 'Employees', icon: 'pi pi-fw pi-users' },
+      { label: 'Presence', icon: 'pi pi-fw pi-calendar' }
+    ];
+    this.activeItem = this.items[0];
+  }
   title = 'employeesPresence';
+
+  getActiveItem(tab: TabMenu){
+    this.activeItem = tab.activeItem;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,30 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PresenceComponent } from './presence/presence.component';
-import { UsersComponent } from './users/users.component';
 import { SettingsComponent } from './settings/settings.component';
+import { CommonModule } from '@angular/common';
+import { TabMenuModule } from 'primeng/tabmenu';
+import { InputTextModule } from 'primeng/inputtext';
+import { EmployeesComponent } from './employees/employees.component';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [
     AppComponent,
     PresenceComponent,
-    UsersComponent,
-    SettingsComponent
+    SettingsComponent,
+    EmployeesComponent
   ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    TabMenuModule,
+    InputTextModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
